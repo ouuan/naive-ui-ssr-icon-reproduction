1f677fb4a79f52ec1883444b347910e96f1f76ad
Yufan You <ouuansteve@gmail.com>
2023-04-13 16:24:40 +0800

    With @huntersofbook/naive-ui-nuxt

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <n-space vertical align="center">
+    <n-result
+      :key="key"
+      title="change key"
+      status="info"
+    />
+    <n-result
+      title="change status"
+      :status="key & 1 ? 'success' : 'error'"
+    />
+    <n-button @click="key += 1">update</n-button>
+  </n-space>
+</template>
+
+<script setup lang="ts">
+const key = ref(0);
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,5 @@
+export default defineNuxtConfig({
+  modules: [
+    '@huntersofbook/naive-ui-nuxt',
+  ],
+})
