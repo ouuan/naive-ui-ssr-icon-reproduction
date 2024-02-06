522f8ba3aaf1138c64165bf66a4744463925ec2d
Yufan You <ouuansteve@gmail.com>
2024-02-06 19:33:36 +0800

    Use @bg-dev/nuxt-naiveui

--- app.vue
+++ app.vue
@@ -1,20 +1,20 @@
 <template>
-  <n-space vertical align="center">
-    <n-result
-      :key="key"
-      title="change key"
-      status="info"
-    />
-    <n-result
-      title="change status"
-      :status="key & 1 ? 'success' : 'error'"
-    />
-    <n-button @click="key += 1">update</n-button>
-  </n-space>
+  <naive-config>
+    <n-space vertical align="center">
+      <n-result
+        :key="key"
+        title="change key"
+        status="info"
+      />
+      <n-result
+        title="change status"
+        :status="key & 1 ? 'success' : 'error'"
+      />
+      <n-button @click="key += 1">update</n-button>
+    </n-space>
+  </naive-config>
 </template>
 
 <script setup lang="ts">
-import { NSpace, NResult, NButton } from 'naive-ui';
-
 const key = ref(0);
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,3 @@
 export default defineNuxtConfig({
-  build: {
-    transpile:
-      process.env.NODE_ENV === 'production'
-        ? [
-            'naive-ui',
-            'vueuc',
-            '@css-render/vue3-ssr',
-            '@juggle/resize-observer'
-          ]
-        : ['@juggle/resize-observer']
-  },
-  vite: {
-    optimizeDeps: {
-      include:
-        process.env.NODE_ENV === 'development'
-          ? ['naive-ui', 'vueuc', 'date-fns-tz/esm/formatInTimeZone']
-          : []
-    }
-  }
+  modules: ["@bg-dev/nuxt-naiveui"],
 })
